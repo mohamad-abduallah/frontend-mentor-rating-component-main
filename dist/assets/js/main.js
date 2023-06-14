797201243a24ce3e682cfb4dae9640b92c29481e
mohamad-abduallah <mohamadn@motory.com>
2023-06-14 14:18:54 +0300

    change the js file and classes name

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -2,35 +2,35 @@
 const rating = document.querySelectorAll('.rate');
 const printRating = document.getElementById('after-rate');
 const beforeRate = document.getElementById('before-rate');
-const  btn = document.querySelector('.submit-rate');
+const btn = document.querySelector('.submit-rate');
 const selectedRatingText = document.getElementById('selectedRating');
 const relatedBtn = document.querySelector('.related-btn');
 
 rating.forEach(item => {
+    btn.disabled = true;
+    btn.classList.add('notSelectedBtn');
     item.addEventListener('click', () => {
         rating.forEach(item => {
             item.classList.remove('selected');
-            btn.classList.remove('selectedBtn');
+            btn.classList.remove('notSelectedBtn');
             btn.disabled = false;
         });
         btn.addEventListener('click' ,() => {
             printRating.style.display = 'flex'
             beforeRate.style.display = 'none';
-
             selectedRatingText.textContent =  item.innerHTML;
+
+            relatedBtn.addEventListener('click' ,() => {
+                printRating.style.display = 'none'
+                beforeRate.style.display = 'block';
+                item.classList.remove('selected');
+                btn.disabled = true;
+                btn.classList.add('notSelectedBtn');
+            });
         });
 
-        relatedBtn.addEventListener('click' ,() => {
-            printRating.style.display = 'none'
-            beforeRate.style.display = 'block';
-
-            item.classList.remove('selected');
-            btn.disabled = true;
-            btn.classList.add('selectedBtn');
-        });
         item.classList.add('selected');
     });
-    btn.classList.add('selectedBtn');
 });
 
 
