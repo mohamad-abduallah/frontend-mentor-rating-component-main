a210b6d6d93aa31e6b897b9da5ab0595f4e8777e
mohamad-abduallah <mohamadn@motory.com>
2023-06-14 13:39:48 +0300

    change the js code and design list

--- project/js/main.js
+++ project/js/main.js
@@ -1,19 +1,36 @@
-function submitRating() {
-    const rating = document.querySelector('input[name="rating"]:checked');
-    const printRating = document.getElementById('after-rate');
-    const beforeRate = document.getElementById('before-rate');
-    const selectedRatingText = document.getElementById('selectedRating');
-    const url = window.location.href;
 
-    if (rating) {
-        const selectedRating = rating.value;
-        selectedRatingText.textContent = selectedRating;
-        setTimeout(() => {
-            printRating.style.display = 'flex';},
-            250);
-        beforeRate.style.display = 'none';
-    } else {
-        printRating.style.display = 'none';
-        beforeRate.style.display = 'block';
-    }
-}
+const rating = document.querySelectorAll('.rate');
+const printRating = document.getElementById('after-rate');
+const beforeRate = document.getElementById('before-rate');
+const  btn = document.querySelector('.submit-rate');
+const selectedRatingText = document.getElementById('selectedRating');
+const relatedBtn = document.querySelector('.related-btn');
+
+rating.forEach(item => {
+    item.addEventListener('click', () => {
+        rating.forEach(item => {
+            item.classList.remove('selected');
+            btn.classList.remove('selectedBtn');
+            btn.disabled = false;
+        });
+        btn.addEventListener('click' ,() => {
+            printRating.style.display = 'flex'
+            beforeRate.style.display = 'none';
+
+            selectedRatingText.textContent =  item.innerHTML;
+        });
+
+        relatedBtn.addEventListener('click' ,() => {
+            printRating.style.display = 'none'
+            beforeRate.style.display = 'block';
+
+            item.classList.remove('selected');
+            btn.disabled = true;
+            btn.classList.add('selectedBtn');
+        });
+        item.classList.add('selected');
+    });
+    btn.classList.add('selectedBtn');
+});
+
+
